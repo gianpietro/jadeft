b397877ccd90c8473f14be059ee73d3a0e60ad92
Gianpietro Signorini <gianpietros@aol.com>
2021-04-27 17:51:38 +0100

    Working file with loop to add subsequent records

--- cur/procur.c
+++ cur/procur.c
@@ -5,6 +5,8 @@
 #include "../inc/procur.h"
 #include "../inc/prolib.h"
 
+
+/* Trim trailing whitespace from the string entered in form field */
 char * trimWS(char *s)
 {
   int i = 0;
@@ -28,103 +30,112 @@ void providerWindow()
   FIELD *providerField[3];
   FORM *providerForm;
   int ch;
-  int cf;   //confirm save data
-  int actInd;
-  char pname[30];  
+  int cf;            /* confirm save data */
+  int actInd;        /* active_Ind */
+  char pname[30];    /* provider_name */ 
   int index , i;
+  int newRec= 'y';   /* Add another new record */
     
   initscr();
   cbreak();
   noecho();
   keypad(stdscr,TRUE);
 
-  /* Add the fields required in the form */
-  /* Size of field rows + cols, upper left corner row + col, offscreen rows, nbuf */
-  providerField[0] = new_field(1, 1, 6, 22, 0, 0);      
-  providerField[1] = new_field(1, 30, 8, 22, 0, 0);
-  providerField[2] = NULL;
- 
-  set_field_type(providerField[0],TYPE_INTEGER,1,1,2);
-  set_field_type(providerField[1],TYPE_REGEXP,"^[A-Za-z0-9 -]+$");
- 
-  providerForm = new_form(providerField);
-  post_form(providerForm);
-
-  mvprintw(2,5,"Jade Finacial Tracker\n");
-  mvprintw(6, 5, "Active Ind:");
-  mvprintw(8, 5, "Provider Name:");  
-  move(6,22);     /* move cursor */
-  refresh();
-
-  while((ch = getch()) != KEY_F(1))
+  while (newRec == 'y')  /* Allows option to add subsequent records to form */
     {
-      switch(ch)
-        {
-	case KEY_DOWN:
-	  form_driver(providerForm, REQ_NEXT_FIELD);
-	  form_driver(providerForm, REQ_END_LINE);
-	  break;
-	case KEY_UP:
-	  form_driver(providerForm, REQ_PREV_FIELD);
-	  form_driver(providerForm, REQ_END_LINE);
-	  break;
-	case KEY_BACKSPACE:
-	  form_driver(providerForm, REQ_CLR_FIELD);
-        case 10:                                           /* ASCII value for new line feed(Enter Key) */
-          form_driver(providerForm, REQ_VALIDATION);
-	  form_driver(providerForm, REQ_NEXT_FIELD);
-	  break;
-        default:
-	 form_driver(providerForm, ch);
-	  break;
-	}
-    }
+      /* Add the fields required in the form */
+      /* Size of field rows + cols, upper left corner row + col, offscreen rows, nbuf */
+      providerField[0] = new_field(1, 1, 6, 22, 0, 0);      
+      providerField[1] = new_field(1, 30, 8, 22, 0, 0);
+      providerField[2] = NULL;
+ 
+      set_field_type(providerField[0],TYPE_INTEGER,1,1,2);
+      set_field_type(providerField[1],TYPE_REGEXP,"^[A-Za-z0-9 -]+$");
+ 
+      providerForm = new_form(providerField);
+      post_form(providerForm);
+ 
+      mvprintw(2,5,"Jade Finacial Tracker\n");
+      mvprintw(6, 5, "Active Ind:");
+      mvprintw(8, 5, "Provider Name:");  
+      move(6,22);     // move cursor 
+      refresh();
 
-  actInd = atoi(field_buffer(providerField[0],0));
-  strcpy(pname, field_buffer(providerField[1],0));
-    
-  int l = strlen(pname);
-  mvprintw(20,5,"pname length %d",l);
-
-  if ((form_driver(providerForm,REQ_VALIDATION) == E_OK) && (actInd >= 1) && (!isspace(*pname)))
-    {
-      strcpy(pname, trimWS(pname));
-      int nl = strlen(pname);
-      mvprintw(22,5,"new pname length %d",nl); 
-      mvprintw(12,5, "Form completed\n");
-      mvprintw(14,5, "Value Active Ind: %d",actInd);
-      mvprintw(15,5,"Value Provider Name: %s", pname);
-      echo();
-      mvprintw(16,5,"Save: y/n :");
-      move(16, 18);
-      while((cf = getch()) != 'y')
+      while((ch = getch()) != KEY_F(1))
 	{
-	  move(16, 18);
-	  if (cf == 'n')
+	  switch(ch)
 	    {
-	      mvprintw(18,5, "Not saved");
+	    case KEY_DOWN:
+	      form_driver(providerForm, REQ_NEXT_FIELD);
+	      form_driver(providerForm, REQ_END_LINE);
+	      break;
+	    case KEY_UP:
+	      form_driver(providerForm, REQ_PREV_FIELD);
+	      form_driver(providerForm, REQ_END_LINE);
+	      break;
+	    case KEY_BACKSPACE:
+	      form_driver(providerForm, REQ_CLR_FIELD);
+	    case 10:                                           /* ASCII value for new line feed(Enter Key) */
+	      form_driver(providerForm, REQ_VALIDATION);
+	      form_driver(providerForm, REQ_NEXT_FIELD);
+	      break;
+	    default:
+	      form_driver(providerForm, ch);
 	      break;
 	    }
 	}
-      if (cf == 'y')
-	{
-	  mvprintw(18,5, "Data saved");
-	  providerInsert(actInd,pname);
-	}
-      noecho();
-   }
-  else
-    {
-      mvprintw(12,5, "Data invalid\n");
-    }
-   
-  getch();
-  
-  unpost_form(providerForm);
-  free_form(providerForm);
-  free_field(providerField[0]);
-  free_field(providerField[1]);
-  free_field(providerField[2]);
 
-  endwin();  
+      actInd = atoi(field_buffer(providerField[0],0));
+      strcpy(pname, field_buffer(providerField[1],0));
+    
+      int l = strlen(pname);
+      mvprintw(20,5,"pname length %d",l);
+
+      if ((form_driver(providerForm,REQ_VALIDATION) == E_OK) && (actInd >= 1) && (!isspace(*pname)))
+	{
+	  strcpy(pname, trimWS(pname));
+	  int nl = strlen(pname);
+	  mvprintw(22,5,"new pname length %d",nl); 
+	  mvprintw(12,5, "Form completed\n");
+	  mvprintw(14,5, "Value Active Ind: %d",actInd);
+	  mvprintw(15,5,"Value Provider Name: %s", pname);
+	  echo();
+	  mvprintw(16,5,"Save: y/n :");
+	  move(16, 18);	  
+	  while((cf = getch()) != 'y')
+	    {
+	      move(16, 18);
+	      if (cf == 'n')
+		{
+		  mvprintw(18,5, "Not saved");
+		  break;
+		}
+	    }	  
+	  if (cf == 'y')
+	    {
+	      mvprintw(18,5, "Data saved");
+	      providerInsert(actInd,pname);   /* Save data to database */
+	    }	  
+	  noecho();
+	}
+      else
+	{
+	  mvprintw(12,5, "Data invalid\n");
+	}
+   
+      getch();
+  
+      unpost_form(providerForm);
+      free_form(providerForm);
+      free_field(providerField[0]);
+      free_field(providerField[1]);
+      free_field(providerField[2]);
+
+      mvprintw(10,5,"Provider Entry Form");
+      mvprintw(12,5,"Do you want to add a new record y/n: ");
+      newRec = getch();      
+    }
+  
+  endwin();
+  
 }
