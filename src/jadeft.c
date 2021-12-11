3f5d98fec4139cb943c3b3e07a667907016b8201
Gianpietro Signorini <gianpietros@aol.com>
2021-12-11 13:09:00 +0000

    Beginning of a menu interface using the cdk_menu widget of ncurses. Able to apply head options with sub-menu options and run appllication depending on item selected.

--- src/jadeft.c
+++ src/jadeft.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libpq-fe.h>
+#include <cdk.h>
 //#include <form.h>
 //#include <ncurses.h>
 //#include <panel.h>
@@ -16,12 +17,66 @@
 #include "../inc/stmuplf.h"
 #include "../inc/stmuplibf.h"
 
-int main (void) {
-  int x;
-  struct statement *ptr;
-  system("clear");   // stdlib.h
-  //  connectToDB();
+static const char *menulist[MAX_MENU_ITEMS][MAX_SUB_ITEMS];
 
+int main (void) {
+   //int x;
+  //struct statement *ptr;
+  //system("clear");
+  
+  CDKSCREEN *cdkscreen = 0;
+  CDKMENU *menu = 0;
+  int submenusize[2], menuloc[2];
+  int selection;
+  char temp[256];
+  const char *mesg[2];
+  initCDKColor();
+ 
+  cdkscreen = initCDKScreen(NULL);
+
+  menulist[0][0] = "</B>File         <!B>";
+  menulist[0][1] = "NOT IN USE       ";
+  menulist[0][2] = "Exit             ";
+
+  menulist[1][0] = "Provider         ";
+  menulist[1][1] = "Provider Type    ";
+  menulist[1][2] = "Provider Insert  ";
+  menulist[1][3] = "Provider Account ";
+
+  submenusize[0] = 3;
+  submenusize[1] = 4;
+
+  menuloc[0] = LEFT;
+  menuloc[1] = LEFT;
+
+  mesg[0]= "                         ";
+
+  menu = newCDKMenu(cdkscreen, menulist, 2, submenusize, menuloc,
+		    TOP, A_UNDERLINE, A_REVERSE);
+
+  refreshCDKScreen(cdkscreen);
+
+  selection = activateCDKMenu(menu, 0);
+
+  while (selection != 1)
+    {
+      switch(selection)
+	{
+	case 100:
+	  provTypeInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 101:
+	  provInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 102:
+	  provAccountInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	}
+    } 
+  
   /*  initscr();  
    cbreak();
  noecho();
@@ -29,9 +84,9 @@ int main (void) {
 
 
   //printw("Lines %d Cols %d\n", LINES, COLS);
-  printf("Enter 1 to continue: ");
+  //  printf("Enter 1 to continue: ");
   //refresh();
-  scanf("%d",&x);
+  // scanf("%d",&x);
   //refresh();
 
   /* provider schema */
@@ -59,14 +114,23 @@ int main (void) {
   //statementLinkInsert();
   
     /*upload stmt functions */
-    ptr = upLoadStatement();
+  /*  ptr = upLoadStatement();
     printStmtFile(ptr);
     statementInsert(ptr); 
-    freeStatement(ptr);
+    freeStatement(ptr); */
+
+  sprintf(temp,"selection %d", selection);
+  mesg[0] = temp;
+  popupLabel(cdkscreen, (CDK_CSTRING2) mesg, 1);
+
+  destroyCDKMenu(menu);
+  destroyCDKScreen(cdkscreen);
+  endCDK();
+
   
-  printf("Completed Enter 1 to exit: ");
+    //  printf("Completed Enter 1 to exit: ");
   //refresh();
-  scanf("%d", &x);
+  //scanf("%d", &x);
 
   //endwin();
   
