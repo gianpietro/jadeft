3b2a5226785d36eac24cf5f57e11f0bdb6547ed8
Gianpietro Signorini <gianpietros@aol.com>
2021-12-12 03:40:02 +0000

    Menu Items Applications
    Applications added to menu for database
    administration, insert, update, delete functions.
    Items include Provider, Supplier, Property, Invoice,
    Statements and other database forms associated with these
    items.

--- src/jadeft.c
+++ src/jadeft.c
@@ -21,12 +21,12 @@ static const char *menulist[MAX_MENU_ITEMS][MAX_SUB_ITEMS];
 
 int main (void) {
    //int x;
-  //struct statement *ptr;
+  struct statement *ptr;
   //system("clear");
   
   CDKSCREEN *cdkscreen = 0;
   CDKMENU *menu = 0;
-  int submenusize[2], menuloc[2];
+  int submenusize[5], menuloc[5];
   int selection;
   char temp[256];
   const char *mesg[2];
@@ -38,20 +38,43 @@ int main (void) {
   menulist[0][1] = "NOT IN USE       ";
   menulist[0][2] = "Exit             ";
 
-  menulist[1][0] = "Provider         ";
+  menulist[1][0] = "Provider Admin   ";
   menulist[1][1] = "Provider Type    ";
   menulist[1][2] = "Provider Insert  ";
   menulist[1][3] = "Provider Account ";
 
+  menulist[2][0] = "Supplier Admin   ";
+  menulist[2][1] = "Supplier         ";
+  menulist[2][2] = "Supplier Type    ";
+  menulist[2][3] = "Supplier Account ";
+  menulist[2][4] = "Invoice          ";
+
+  menulist[3][0] = "Other Admin      ";
+  menulist[3][1] = "Property         ";
+  menulist[3][2] = "Payment period   ";
+  menulist[3][3] = "Category         ";
+  menulist[3][4] = "Statement Config ";
+  menulist[3][5] = "Statement Upload ";
+
+  menulist[4][0] = "Documents        ";
+  menulist[4][1] = "Document Type    ";
+  menulist[4][2] = "Document Insert  ";
+  
   submenusize[0] = 3;
   submenusize[1] = 4;
+  submenusize[2] = 5;
+  submenusize[3] = 6;
+  submenusize[4] = 3;
 
   menuloc[0] = LEFT;
   menuloc[1] = LEFT;
+  menuloc[2] = LEFT;
+  menuloc[3] = LEFT;
+  menuloc[4] = LEFT;
 
   mesg[0]= "                         ";
 
-  menu = newCDKMenu(cdkscreen, menulist, 2, submenusize, menuloc,
+  menu = newCDKMenu(cdkscreen, menulist, 5, submenusize, menuloc,
 		    TOP, A_UNDERLINE, A_REVERSE);
 
   refreshCDKScreen(cdkscreen);
@@ -73,6 +96,51 @@ int main (void) {
 	case 102:
 	  provAccountInsert();
 	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 200:
+	  suppInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 201:
+	  suppTypeInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 202:
+	  suppAccountInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 300:
+	  propertyInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 301:
+	  paymentPeriodInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 302:
+	  categoryInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 303:
+	  statementLinkInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 304:
+	  ptr = upLoadStatement();
+	  printStmtFile(ptr);
+	  statementInsert(ptr); 
+	  freeStatement(ptr);
+	  selection = activateCDKMenu(menu, 0);
+	case 400:
+	  documentTypeInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	case 401:
+	  documentInsert();
+	  selection = activateCDKMenu(menu, 0);
+	  break;
+	default:
+	  sprintf(temp,"No selection");
 	  break;
 	}
     } 
