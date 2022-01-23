a2572335ac54a1cf0b0985cc21ad47b153ea9f81
Gianpietro Signorini <gianpietros@aol.com>
2022-01-23 12:56:42 +0000

    Statement audit - select provider and date range input

--- cur/stmcur.c
+++ cur/stmcur.c
@@ -15,34 +15,29 @@
    and delete values */
 void stmtDataAudit()
 {
-  WINDOW *mainWin,*subOneWin, *subTwoWin, *updateWin;
+  WINDOW *mainWin, *updateWin;
   FORM *mainForm;
   FIELD *inputField[8];
-  PANEL *mainPanel, *subOnePanel, *subTwoPanel, *updatePanel;
+  PANEL *mainPanel,*updatePanel;
   int list = 2, i = 0, j = 0;
   char p;
-  int ch, mrow, mcol, s1row, s1col, s2row, s2col, urow, ucol;
-  int subOneID, subTwoID;
-  char subOneIDstr[5];  
-  char subTwoIDstr[5];  
+  int ch, mrow, mcol, urow, ucol;
   int rows; 
   int upID;
   int val, *params[1], length[1], formats[1];   //PQexecParams 
-  int fv0, fv1, fv3, fv5;  // field values
-  char fv2[30], fv4[30];
-  float XXXXamount;
+  int fv1;  // field values
+  char fv2[4], fv3[150], fv5[50], fv6[30];
+  float fv4;
   int cf; // confirm save to DB
   int newRec = 'y';
   int cfUpdate = 0;
   const char *titleOne = "Statement Form";
-  const char *titleTwo = "Provider List";
-  const char *titleThree = "Provider Type";
   const char *titleFour = "Provider Account";  
   int lenOne = strlen(titleOne);
-  int lenTwo = strlen(titleTwo);
-  int lenThree = strlen(titleThree);
   int lenFour = strlen(titleFour);
   int fldColor = 0;
+  int df= 0; //date from
+  int dt = 0; //date to
 
   PGconn *conn =  fdbcon();
   PGresult *res;
@@ -96,12 +91,12 @@ void stmtDataAudit()
       scale_form(mainForm, &mrow, &mcol);   
 
       mainWin = newwin(LINES-15, COLS/3,LINES-(LINES-4),COLS/15);
-      subOneWin = newwin((LINES-10)/2, COLS/3,LINES-(LINES-4),COLS/2);
-      subTwoWin = newwin((LINES-10)/2, COLS/3,LINES-(LINES-4),COLS/2);
-      updateWin = newwin((LINES-10)/2, COLS/3,LINES-(LINES-4),COLS/2);
+      //subOneWin = newwin((LINES-10)/2, COLS/3,LINES-(LINES-4),COLS/2);
+      //subTwoWin = newwin((LINES-10)/2, COLS/3,LINES-(LINES-4),COLS/2);
+      updateWin = newwin(LINES*0.5, COLS/3,LINES-(LINES-4),COLS/2);
 
-      subOnePanel = new_panel(subOneWin);      
-      subTwoPanel = new_panel(subTwoWin);
+      //subOnePanel = new_panel(subOneWin);      
+      //subTwoPanel = new_panel(subTwoWin);
       updatePanel = new_panel(updateWin);
       mainPanel = new_panel(mainWin);
       wbkgd(mainWin, COLOR_PAIR(1));     
@@ -109,25 +104,25 @@ void stmtDataAudit()
       doupdate();
       
       keypad(mainWin, TRUE);
-      keypad(subOneWin, TRUE);
-      keypad(subTwoWin, TRUE);
+      //keypad(subOneWin, TRUE);
+      //keypad(subTwoWin, TRUE);
       keypad(updateWin, TRUE);
 
       set_form_win(mainForm,mainWin);
       set_form_sub(mainForm, derwin(mainWin,mrow,mcol,2,2));      
       getmaxyx(mainWin, mrow, mcol);
-      getmaxyx(subOneWin, s1row, s1col);
-      getmaxyx(subTwoWin, s2row, s2col);
+      //getmaxyx(subOneWin, s1row, s1col);
+      //getmaxyx(subTwoWin, s2row, s2col);
       getmaxyx(updateWin, urow, ucol);
       box(mainWin,0,0);
-      box(subOneWin,0,0);
-      box(subTwoWin,0,0);
+      //box(subOneWin,0,0);
+      //box(subTwoWin,0,0);
       box(updateWin,0,0);
       wattron(mainWin,A_BOLD | COLOR_PAIR(1));     
       mvwprintw(mainWin,1,(mcol-lenOne)/2,titleOne);  
       wattroff(mainWin,A_BOLD | COLOR_PAIR(1));   
 
-      if(mainWin == NULL || subOneWin == NULL || subTwoWin == NULL || updateWin == NULL)
+      if(mainWin == NULL || updateWin == NULL)
 	{
 	  endwin();
 	  puts("Unable to create window");
@@ -150,191 +145,13 @@ void stmtDataAudit()
   
       while((ch = wgetch(mainWin)) != KEY_F(1))
 	{
-	  hide_panel(subOnePanel);
-	  hide_panel(subTwoPanel);
+	  //hide_panel(subOnePanel);
+	  //hide_panel(subTwoPanel);
 	  hide_panel(updatePanel);
 	  show_panel(mainPanel);
 	  update_panels();
 	  doupdate();	  
 	  keyNavigate(ch, mainForm);
-	  if(ch == KEY_F(2))
-	    {
-	      i = j = rows = 0;
-	      list = 6;
-	      wclear(subOneWin);
-	      box(subOneWin,0,0);
-	      wattron(subOneWin,A_BOLD | COLOR_PAIR(1));    
-	      mvwprintw(subOneWin,1,(s1col-lenTwo)/2, titleTwo); 
-	      wattroff(subOneWin,A_BOLD | COLOR_PAIR(1));   
-	      wmove(subOneWin,1,1);	   
-	      show_panel(subOnePanel);
-	      wbkgd(subOneWin, COLOR_PAIR(1));          
-	      update_panels();
-	      doupdate();
-	  
-	      res = PQexec(conn,"SELECT * FROM provider WHERE active_ind = 1 ORDER BY provider_id");	  
-	      rows = PQntuples(res);
-
-	      wrefresh(subOneWin);
-	      wattron(subOneWin,A_BOLD | COLOR_PAIR(1));
-	      mvwprintw(subOneWin, 4, 1, "ID    Name"); 
-	      wattroff(subOneWin,A_BOLD | COLOR_PAIR(1)); 
-	      while((p = wgetch(subOneWin)) == '\n')
-		{
-		  if ( j + RANGE < rows)
-		    j = j + RANGE;	
-		  else
-		    j = j + (rows - j);
-		  for (i; i < j; i++)
-		    {	 
-		      mvwprintw(subOneWin,list,1,"%-5s %-25s", PQgetvalue(res,i,0),PQgetvalue(res,i,2));
-		      list++;
-		      wclrtoeol(subOneWin);
-		      box(subOneWin,0,0);   
-		    }
-		  list = 6;      	
-		  if  (i == rows)
-		    {
-		      wclrtobot(subOneWin);  
-		      mvwprintw(subOneWin,13,1,"End of list");
-		      box(subOneWin,0,0);
-		      wattron(subOneWin,A_BOLD | COLOR_PAIR(1));       
-		      mvwprintw(subOneWin,1,(s1col-lenTwo)/2, titleTwo);
-		      wattroff(subOneWin,A_BOLD | COLOR_PAIR(1)); 
-		      wmove(subOneWin,10,1);
-		      break;
-		    }
-		}	  
-	      echo();
-	      wattron(subOneWin,A_BOLD | COLOR_PAIR(1));          
-	      mvwprintw(subOneWin,s1row-7,1,"Select Option: ");
-	      mvwscanw(subOneWin,s1row-7,s1col-45, "%5s", &subOneIDstr);
-	      set_field_buffer(inputField[1],0, subOneIDstr);
-              wattroff(subOneWin,A_BOLD | COLOR_PAIR(1));         
-	      subOneID = atoi(field_buffer(inputField[1],0));
-	      PQclear(res);
-	  
-	      val = htonl((uint32_t)subOneID);
-	      params[0] = (int *)&val;
-	      length[0] = sizeof(val);
-	      formats[0] = 1;
-
-	      res = PQexecParams(conn, "SELECT * FROM provider WHERE provider_id = $1;"
-				 ,1
-				 ,NULL
-				 ,(const char *const *)params
-				 ,length
-				 ,formats
-				 ,0);
-	  
-	      rows = PQntuples(res);
-	      if (rows == 1)
-		{
-		  set_field_buffer(inputField[1],0,PQgetvalue(res,0,0));
-		  wrefresh(subOneWin);
-		}
-	      else
-		{
-		  set_field_buffer(inputField[1],0, "");
-		  wattron(subOneWin,A_BOLD | COLOR_PAIR(1));   
-		  mvwprintw(subOneWin,s1row-6,1,"Number invalid");
-		  wattroff(subOneWin,A_BOLD | COLOR_PAIR(1));  
-		  wrefresh(subOneWin);		
-		}
-	      noecho();
-	      PQclear(res);
-	    }
-	  if(ch == KEY_F(3))
-	    {
-	      i = j = rows = 0;
-	      list = 6;
-	      wclear(subTwoWin);
-	      box(subTwoWin,0,0);
-	      wattron(subTwoWin,A_BOLD | COLOR_PAIR(1));   
-	      mvwprintw(subTwoWin,1,(s1col-lenThree)/2, titleThree); 
-	      wattroff(subTwoWin,A_BOLD | COLOR_PAIR(1));   
-	      wmove(subTwoWin,1,1);
-	      show_panel(subTwoPanel);
-	      wbkgd(subTwoWin, COLOR_PAIR(1));         
-	      update_panels();
-	      doupdate();
-	      wrefresh(subTwoWin);
-
-	      res = PQexec(conn,"SELECT * FROM provider_type ORDER BY provider_type_id");
-	      rows = PQntuples(res);
-
-	      wrefresh(subTwoWin);
-	      wattron(subTwoWin,A_BOLD | COLOR_PAIR(1));
-	      mvwprintw(subTwoWin, 4, 1, "ID    Description"); 
-	      wattroff(subTwoWin,A_BOLD | COLOR_PAIR(1));   
-
-	      while((p = wgetch(subTwoWin)) == '\n')
-		{
-		  if ( j + RANGE < rows)
-		    j = j + RANGE;	
-		  else
-		    j = j + (rows - j);
-		  for (i; i < j; i++)
-		    {	 
-		      mvwprintw(subTwoWin,list,1,"%-5s %-25s", PQgetvalue(res,i,0),PQgetvalue(res,i,1));
-		      list++;
-		      wclrtoeol(subTwoWin);
-		      box(subTwoWin,0,0);    
-		    }
-		  list = 6;      
-
-		  if  (i == rows)
-		    {
-		      wclrtobot(subTwoWin);  
-		      mvwprintw(subTwoWin,13,1,"End of list");
-		      box(subTwoWin,0,0);		     
-		      wattron(subTwoWin,A_BOLD | COLOR_PAIR(1)); 
-		      mvwprintw(subTwoWin,1,(s1col-lenThree)/2, titleThree);   
-		      wattroff(subTwoWin,A_BOLD | COLOR_PAIR(1));  
-		      wmove(subTwoWin,10,1);
-		      break;
-		    }
-		}
-	      echo();
-	      wattron(subTwoWin,A_BOLD | COLOR_PAIR(1));           
-	      mvwprintw(subTwoWin,s2row-7,1,"Select Provider: ");
-	      mvwscanw(subTwoWin,s2row-7,s2col-45, "%5s", &subTwoIDstr);
-	      set_field_buffer(inputField[5],0, subTwoIDstr);
-	      wattroff(subTwoWin,A_BOLD | COLOR_PAIR(1));          
-
-	      subTwoID = atoi(field_buffer(inputField[5],0));
-	      PQclear(res);
-
-	      val = htonl((uint32_t)subTwoID);
-	      params[0] = (int *)&val;
-	      length[0] = sizeof(val);
-	      formats[0] = 1;
-
-	      res = PQexecParams(conn, "SELECT * FROM provider_type WHERE provider_type_id = $1;"
-				 ,1
-				 ,NULL
-				 ,(const char *const *)params
-				 ,length
-				 ,formats
-				 ,0);
-	  
-	      rows = PQntuples(res);
-	      if (rows == 1)
-		{
-		  set_field_buffer(inputField[5],0,PQgetvalue(res,0,0));
-		  wrefresh(subTwoWin);
-		}
-	      else
-		{
-		  set_field_buffer(inputField[5],0,"");
-		  wattron(subTwoWin,A_BOLD | COLOR_PAIR(1));    
-		  mvwprintw(subTwoWin,s2row-6,1,"Number invalid");
-		  wattroff(subTwoWin,A_BOLD | COLOR_PAIR(1));   
-		  wrefresh(subTwoWin);		
-		}
-	      noecho();
-	      PQclear(res);
-	    }  // if (ch == f3)
 	  if(ch == KEY_F(9))
 	    {
 	      i = j = rows = 0, cfUpdate = 0;
@@ -342,7 +159,7 @@ void stmtDataAudit()
 	      wclear(updateWin);
 	      box(updateWin,0,0);
 	      wattron(updateWin,A_BOLD | COLOR_PAIR(1));   
-	      mvwprintw(updateWin,1,(s1col-lenFour)/2, titleFour);   
+	      mvwprintw(updateWin,1,(ucol-lenFour)/2, titleFour);   
 	      wattroff(updateWin,A_BOLD | COLOR_PAIR(1));   
 	      wmove(updateWin,1,1);
 	      show_panel(updatePanel);
@@ -375,10 +192,10 @@ void stmtDataAudit()
 		  if  (i == rows)
 		    {
 		      wclrtobot(updateWin);  
-		      mvwprintw(updateWin,13,1,"End of list");
+		      mvwprintw(updateWin,urow-15,1,"End of list");
 		      box(updateWin,0,0);
 		      wattron(updateWin,A_BOLD | COLOR_PAIR(1));  
-	              mvwprintw(updateWin,1,(s1col-lenFour)/2, titleFour);  
+	              mvwprintw(updateWin,1,(ucol-lenFour)/2, titleFour);  
 	              wattroff(updateWin,A_BOLD | COLOR_PAIR(1));   
 		      wmove(updateWin,10,1);
 		      break;
@@ -386,8 +203,8 @@ void stmtDataAudit()
 		}	  
 	      echo();
 	      wattron(updateWin,A_BOLD | COLOR_PAIR(1));           
-	      mvwprintw(updateWin,urow-7,1,"Select Option: ");
-	      mvwscanw(updateWin,urow-7, ucol-45, "%d", &upID);
+	      mvwprintw(updateWin,urow-14,1,"Select Option: ");  //urow-7
+	      mvwscanw(updateWin,urow-14, ucol-45, "%d", &upID);
 	      wattroff(updateWin,A_BOLD | COLOR_PAIR(1));           
 
 	      PQclear(res);
@@ -408,18 +225,27 @@ void stmtDataAudit()
 	      rows = PQntuples(res);
 	      if (rows == 1)
 		{
-		  set_field_buffer(inputField[0],0,PQgetvalue(res,0,1));
+		  /*set_field_buffer(inputField[0],0,PQgetvalue(res,0,1));
 		  set_field_buffer(inputField[1],0,PQgetvalue(res,0,2));
 		  set_field_buffer(inputField[2],0,PQgetvalue(res,0,3));
 		  set_field_buffer(inputField[3],0,PQgetvalue(res,0,4));
 		  set_field_buffer(inputField[4],0,PQgetvalue(res,0,5));
-		  set_field_buffer(inputField[5],0,PQgetvalue(res,0,6));
+		  set_field_buffer(inputField[5],0,PQgetvalue(res,0,6));*/
+		  strcpy(fv5, trimWS(field_buffer(inputField[5],0)));
+
+		  mvwprintw(updateWin, urow-11,1,"Date From:");
+		  mvwscanw(updateWin,urow-11, ucol-45, "%d", &df);
+		  mvwprintw(updateWin, urow-10,1,"Date To:");
+		  mvwscanw(updateWin,urow-10, ucol-45, "%d", &df);
 		  cfUpdate = 1;
+		  hide_panel(updatePanel);
+		  update_panels();
+		  doupdate();
 		}
 	      else
 		{
 		  wattron(updateWin,A_BOLD | COLOR_PAIR(1));       
-		  mvwprintw(updateWin,urow-6,1,"Number invalid");
+		  mvwprintw(updateWin,urow-13,1,"Number invalid");
 		  wrefresh(updateWin);
 		  wattroff(updateWin,A_BOLD | COLOR_PAIR(1));      
 		}
@@ -427,8 +253,8 @@ void stmtDataAudit()
 	      PQclear(res);
 	    } //F9	  
 	}//while F1
-      hide_panel(subOnePanel);
-      hide_panel(subTwoPanel);
+      //hide_panel(subOnePanel);
+      //hide_panel(subTwoPanel);
       hide_panel(updatePanel);
       update_panels();
       doupdate();
@@ -436,12 +262,12 @@ void stmtDataAudit()
       /* code goes here for assign buffer value and validate prior to insert */
       form_driver(mainForm,REQ_VALIDATION);
 
-      fv0 = atoi(field_buffer(inputField[0],0));
+      /*fv0 = atoi(field_buffer(inputField[0],0));
       fv1 = atoi(field_buffer(inputField[1],0));
       strcpy(fv2,trimWS(field_buffer(inputField[2],0)));
       fv3 = atoi(field_buffer(inputField[3],0));
       strcpy(fv4, trimWS(field_buffer(inputField[4],0)));
-      fv5 = atoi(field_buffer(inputField[5],0));
+      strcpy(fv5, trimWS(field_buffer(inputField[5],0)));*/
 
       if ((form_driver(mainForm,REQ_VALIDATION) == E_OK) && (fv5 >= 0))
 	{
@@ -510,7 +336,7 @@ void stmtDataAudit()
       echo();
       while((newRec = wgetch(mainWin)) != 'y')
 	{
-	  wmove(mainWin,mrow-6,mcol-27);  //CHECK COL POSITION OK
+	  wmove(mainWin,mrow-6,mcol-27);  
 	  if(newRec == 'n')
 	    break;
 	}
