2b4741d5c1bc697dfa968ba0e096d726a3911a30
Gianpietro Signorini <gianpietros@aol.com>
2022-07-24 08:54:48 +0100

    stmuplibf.h - code layout

--- inc/stmuplibf.h
+++ inc/stmuplibf.h
@@ -1,19 +1,19 @@
 #ifndef _STMUPLIBF_H
 #define _STMUPLIBF_H
 
-#define QM 34                                                            /* quotation mark */
-#define SP 32                                                            /* space */
-#define CM 44                                                            /* comma */
-#define FS 47                                                            /* forward slash */
-#define AP 39                                                            /* apostrophe */
-#define TDATE 9                                                          /* transaction date */
-#define TTYPE 4                                                          /* transaction type */
-#define TDESC 150                                                        /* transaction description */
-#define TVALUE 11                                                        /* transaction value */
-#define ANUM 17                                                          /* account number */
-#define ALIAS 51                                                         /* alias matched from statement_link table */
-#define FNAME 71                                                         /* filename of statement file to upload */
-#define BACKSP 7                                                         /* backspace infStmtName is ascii  7 (?127)*/
+#define QM 34                                                                   /* quotation mark */
+#define SP 32                                                                   /* space */
+#define CM 44                                                                   /* comma */
+#define FS 47                                                                   /* forward slash */
+#define AP 39                                                                   /* apostrophe */
+#define TDATE 9                                                                 /* transaction date */
+#define TTYPE 4                                                                 /* transaction type */
+#define TDESC 150                                                               /* transaction description */
+#define TVALUE 11                                                               /* transaction value */
+#define ANUM 17                                                                 /* account number */
+#define ALIAS 51                                                                /* alias matched from statement_link table */
+#define FNAME 71                                                                /* filename of statement file to upload */
+#define BACKSP 7                                                                /* backspace infStmtName is ascii  7 (?127)*/
 
 
 struct statement{
